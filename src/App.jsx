5f5150afeabebf01929c0ac1830c2d366133283a
metehannolgun <metehannolgun@gmail.com>
2025-02-25 20:25:18 +0300

    Navbar componenti bitti. -Navbarın altında gösterilcek olan Hero componenti oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar.jsx'
+import Hero from './components/Hero/Hero.jsx'
 const App = () => {
   return (
       <main className=''>
         <Navbar />
+        <Hero />
       </main>
+
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import Logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
     <header>
-        <div className='container py-3'>
+        <div className='container py-3 flex items-center justify-items-center justify-between'>
             {/* Logo Section */}
             <div>
-                <img src={Logo} alt='' className='w-36'/>
+                <img src={Logo} alt='' className='w-40'/>
             </div>
             {/* Navlinks Section */}
-            <ul>
+            <ul className='hidden md:flex items-center gap-5'>
                 <li>
                     <a href='#'>Products</a>
                 </li>
@@ -25,7 +25,7 @@ const Navbar = () => {
                 </li>
             </ul>
             {/* Button  Section */}
-            <button>Get In Touch</button>
+            <button className='border border-gray-500 px-4 py-2 rounded-lg hover:bg-red-400 hover:text-white transform duration-300 '>Get In Touch</button>
         </div>
     </header>
   )
