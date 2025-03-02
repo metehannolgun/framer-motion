e26a9f269b731d1888ad953e761aec0a029c0f15
metehannolgun <metehannolgun@gmail.com>
2025-03-02 19:43:48 +0300

    Hero componentine framer eklendi

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import HeroImage from '../../assets/hero.png'
+import React from "react";
+import HeroImage from "../../assets/hero.png";
+import { SlideUp } from "../../utility/animation";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -8,22 +10,39 @@ const Hero = () => {
         {/* Text Section */}
         <div className='flex flex-col justify-center xl:pr-40'>
           <div className='mt-24 mb-10 md:mt-0 md:mb-0 space-y-6 text-center md:text-left md:pl-8'>
-            <h1 className='text-5xl font-bold text-blue-900'>COVID-19 Risk <br/> Assessment Tool</h1>
-            <p className='text-lg text-gray-400 mt-4'>
+            <motion.h1 
+            variants={SlideUp(0.2)}
+            whileInView={"animate"}
+            initial="initial"
+            className='text-5xl font-bold text-blue-900'>COVID-19 Risk <br/> Assessment Tool
+            </motion.h1>
+            <motion.p
+            variants={SlideUp(0.4)}
+            whileInView={"animate"}
+            initial="initial" 
+            className='text-lg text-gray-400 mt-4'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quam, quisquam temporibus saepe deleniti expedita officia delectus ratione repellat. Ducimus cupiditate recusandae dignissimos aliquid nemo!
-            </p>
-            <div className='text-center md:text-left'>
+            </motion.p>
+            <motion.div 
+            variants={SlideUp(0.6)}
+            whileInView={"animate"}
+            initial="initial"
+            className='text-center md:text-left'>
               <div className='bg-pink-500 text-white px-6 py-4 rounded-lg mt-4 w-fit font-bold hover:shadow-lg transform duration-300 mx-auto md:mx-0'>
                 <button>See how to use it</button>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
 
         {/* Image Section */}
-        <div className='flex items-center justify-center'>
+        <motion.div 
+        initial={{ opacity: 0, scale: 0 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5, delay: 0.6 }}
+        className='flex items-center justify-center'>
           <img src={HeroImage} alt="" />
-        </div>
+        </motion.div>
       </div>
     </section>
   )
